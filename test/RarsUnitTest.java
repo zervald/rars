e42451a0783f87051da922990a0d46e65d9a6d1d
Jean Privat <jean@pryen.org>
2024-07-30 01:26:13 -0400

    run rars test as junit tests

---
+++ test/RarsUnitTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+public class RarsUnitTest extends RarsTest {
+    @Override
+    @Test
+    public void checkBinary() {
+        super.checkBinary();
+    }
+
+    @Override
+    @Test
+    public void checkPseudo() {
+        super.checkPseudo();
+    }
+
+    @Override
+    @Test
+    public void checkPrograms() {
+        super.checkPrograms();
+    }
+}
